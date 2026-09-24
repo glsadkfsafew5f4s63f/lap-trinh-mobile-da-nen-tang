5a2c3ff9679964cd8706804200fc5a09a830af91
ThanDucHuy <thanduchuy358@gmail.com>
2026-09-24 07:49:14 +0700

    Delete Fe directory

--- Fe/assets/README.txt
+++
@@ -1,2 +0,0 @@
-Thư mục ảnh của ứng dụng (icon, splash, ảnh sản phẩm sau này).
-Hiện tại app đang dùng ảnh mẫu từ internet trong data/products.ts.
--- Fe/data/address.ts
+++
@@ -1,5 +0,0 @@
-export const defaultAddress = {
-  name: 'Nguyễn Văn A',
-  phone: '0901234567',
-  address: '123 Nguyễn Trãi, Phường Bến Thành, Quận 1, TP.HCM',
-};
